6488c7a38bfb83e62eb2b71513a6b19d5dbd848a
Unknown <andrieu15@hotmail.com>
2018-10-25 16:54:27 +0200

    Test

--- src/main/java/model/Vol.java
+++ src/main/java/model/Vol.java
@@ -35,7 +35,78 @@ public class Vol {
 	@Temporal(TemporalType.TIMESTAMP)
 	private static Date heureArrivee;
 	@Version
+
 	private Integer version;
+	
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getVersion() {
+		return version;
+	}
+	public void setVersion(Integer version) {
+		this.version = version;
+	}
+	public Date getDateDepart() {
+		return DateDepart;
+	}
+	public void setDateDepart(Date dateDepart) {
+		DateDepart = dateDepart;
+	}
+	public Date getDateArrivee() {
+		return DateArrivee;
+	}
+	public void setDateArrivee(Date dateArrivee) {
+		DateArrivee = dateArrivee;
+	}
+	public static Date getHeureDepart() {
+		return heureDepart;
+	}
+	public static void setHeureDepart(Date heureDepart) {
+		Vol.heureDepart = heureDepart;
+	}
+	public static Date getHeureArrivee() {
+		return heureArrivee;
+	}
+	public static void setHeureArrivee(Date heureArrivee) {
+		Vol.heureArrivee = heureArrivee;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Vol other = (Vol) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	public Vol() {
+		super();
+	}
+	public Vol(Date dateDepart, Date dateArrivee) {
+		super();
+		DateDepart = dateDepart;
+		DateArrivee = dateArrivee;
+	}
 	
 	
 //	@Transient
