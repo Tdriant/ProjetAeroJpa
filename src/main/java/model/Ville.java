a441eae5a8ded754c96645225a7e17137898484e
crognier <nemo.crognier@gmail.com>
2018-10-25 15:44:29 +0200

    commit initial de ville sans annotations

---
+++ src/main/java/model/Ville.java
@@ -0,0 +1,55 @@
+package model;
+
+public class Ville {
+	private String nom;
+	private Integer id;
+	public Ville() {
+
+		// TODO Auto-generated constructor stub
+	}
+	public Ville(String nom, Integer id) {
+		super();
+		this.nom = nom;
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Ville other = (Ville) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	
+	
+	
+
+}
