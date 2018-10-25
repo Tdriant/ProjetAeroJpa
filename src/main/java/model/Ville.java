6023cb39654faf873bcdabe91ee88aab4eb7b4a2
crognier <nemo.crognier@gmail.com>
2018-10-25 16:21:13 +0200

    ville corrigée

--- src/main/java/model/Ville.java
+++ src/main/java/model/Ville.java
@@ -1,29 +1,53 @@
 package model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Version;
+
+@Entity
+@Table(name = "villes")
+@SequenceGenerator(name = "seqVille", sequenceName = "seq_ville", initialValue = 50, allocationSize = 1)
 public class Ville {
-	private String nom;
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seqVille")
 	private Integer id;
+	@Column(name = "ville", length = 150)
+	private String nom;
+	@Version
+	private int version;
+
 	public Ville() {
 
 		// TODO Auto-generated constructor stub
 	}
+
 	public Ville(String nom, Integer id) {
 		super();
 		this.nom = nom;
 		this.id = id;
 	}
+
 	public String getNom() {
 		return nom;
 	}
+
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -31,6 +55,7 @@ public class Ville {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -47,9 +72,5 @@ public class Ville {
 			return false;
 		return true;
 	}
-
-	
-	
-	
 
 }
