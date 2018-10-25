95fa9cf6f1b72e5f50c28db2105073bc8a2151dd
crognier <nemo.crognier@gmail.com>
2018-10-25 17:46:18 +0200

    VilleAeroport classe dao et key

---
+++ src/main/java/dao/DaoVilleAeroportJpaImpl.java
@@ -0,0 +1,46 @@
+package dao;
+
+import java.util.List;
+
+import model.VilleAeroport;
+import model.VilleAeroportKey;
+
+public class DaoVilleAeroportJpaImpl implements DaoVilleAeroport {
+
+	@Override
+	public void create(VilleAeroport obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public VilleAeroport findByKey(VilleAeroportKey key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public VilleAeroport update(VilleAeroport obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(VilleAeroport obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteByKey(VilleAeroportKey key) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<VilleAeroport> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/model/VilleAeroport.java
@@ -0,0 +1,50 @@
+package model;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "VilleAeroport")
+public class VilleAeroport {
+	@EmbeddedId
+	private VilleAeroport key;
+
+	public VilleAeroport() {
+
+	}
+
+	public VilleAeroport getKey() {
+		return key;
+	}
+
+	public void setKey(VilleAeroport key) {
+		this.key = key;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((key == null) ? 0 : key.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		VilleAeroport other = (VilleAeroport) obj;
+		if (key == null) {
+			if (other.key != null)
+				return false;
+		} else if (!key.equals(other.key))
+			return false;
+		return true;
+	}
+
+}
